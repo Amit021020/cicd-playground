bcc2a8e327783d051f513068b9b4d38f04a49b7f
Amit <amitsuyal09@gmail.com>
2026-05-14 17:03:33 +0530

    solved a problem

---
+++ config/config.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const config = {
+    port: process.env.PORT || 3000,
+
+    mongoURI: process.env.MONGO_URI,
+
+    jwtSecret: process.env.JWT_SECRET,
+
+    docker: {
+        baseImagePrefix: "deploy",
+        tempDir: "/tmp"
+    }
+};
+
+module.exports = config;
